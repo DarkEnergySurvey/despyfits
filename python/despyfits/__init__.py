09768987b2bcf8d128bca130e7615b4f7ddcaa45
Felipe Menanteau <felipe@illinois.edu>
2015-02-04 20:41:07 +0000

    reverting trunk to rev 32921

---
+++ python/despyfits/__init__.py
@@ -0,0 +1,11 @@
+__author__  = "Felipe Menanteau"
+__version__ = '0.1.1'
+version = __version__
+
+"""
+ A collection of FITS files-related Python functions useful for DESDM
+ Python-based modules
+"""
+
+from . import fitsutils
+from .fitsutils import *
