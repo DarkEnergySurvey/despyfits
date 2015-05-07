928b166fa98e68592b86da1a8595eb6f912a7f52
Felipe Menanteau <felipe@illinois.edu>
2015-05-07 18:21:17 +0000

    update version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ libmaskbits = SharedLibrary(
 
 # The main call
 setup(name='despyfits',
-      version ='0.2.0',
+      version ='0.2.2',
       license = "GPL",
       description = "A set of handy Python fitsfile-related utility functions for DESDM",
       author = "Felipe Menanteau, Eric Neilsen",
