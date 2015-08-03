c42ca2d7fdcd23862b04644ada5446ce4d23a579
Felipe Menanteau <felipe@illinois.edu>
2015-08-03 22:07:06 +0000

    Fixed printHeader to be compatible with old/new pyfits and stdout handle

--- bin/printHeader.py
+++ bin/printHeader.py
@@ -12,25 +12,41 @@ Print header values to either stdout or to a file
 import argparse
 import os
 import sys
+import pyfits
 from pyfits import getheader
 
 def print_header(fitsfile,ofileh=sys.stdout):
     """ print header from fits file to either stdout or to a file """
+
+    # Get the Pyfits version as a float
+    pyfitsVersion = float(".".join(pyfits.__version__.split(".")[0:2]))
+
     hdr = getheader(fitsfile)
-    ofileh.write("%s" % hdr.ascardlist())
+    if pyfitsVersion < 3.1:  # Older method
+        ofileh.write("%s" % hdr.ascardlist())
+    else: # use the up-to-date method (Header.cards)
+        for items in hdr.cards:
+            ofileh.write("%s\n" % items)
+
+    ofileh.write("\n")
+    outfh.close()
+    return
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Prints fits headers')
-    parser.add_argument('-O', '--outfile', action='store', type=str, help="Print header to given file", default=sys.stdout)
+    parser.add_argument('-O', '--outfile', action='store', type=str,help="Print header to given file", default=False)
     #parser.add_argument('-c', action='store', type=str, help="Text file containing which header values to print")
     parser.add_argument('fitsfile', action='store')
     args = parser.parse_args()
     
-    outfh=sys.stdout
-    if args.outfile is not None:
-        outfh = open(args.outfile, "w")
+    if args.outfile:
+        try:
+            outfh = open(args.outfile, "w")
+        except:
+            sys.exit("ERROR: Cannot open %s" % args.outfile)
+    else:
+        outfh=sys.stdout
 
+    # Make the call
     print_header(args.fitsfile, outfh)
-
-    if args.outfile is not None:
-        outfh.close()
