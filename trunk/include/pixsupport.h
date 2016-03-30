7f3103ddb198f615194acda1584407d65c13b541
Felipe Menanteau <felipe@illinois.edu>
2016-03-30 19:39:23 +0000

    removing trunk from tags/0.3.2

--- trunk/include/pixsupport.h
+++
@@ -1,3 +0,0 @@
-#define Squ(x) ((x)*(x))
-#define Cube(x) ((x)*(x)*(x))
-#define Quad(x) (Squ(x)*Squ(x))
--- trunk/python/despyfits/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__  = "Felipe Menanteau, Eric Neilsen"
-__version__ = '0.3.3'
-version = __version__
-
-"""
- A collection of FITS files-related Python functions useful for DESDM
- Python-based modules
-"""
-
