d394ea86192388899712cb79172f093a030bd209
Felipe Menanteau <felipe@illinois.edu>
2016-05-03 20:54:28 +0000

    Tagging despyfits as 0.3.5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ libcompressionhdu = SharedLibrary(
 
 # The main call
 setup(name='despyfits',
-      version ='0.3.4',
+      version ='0.3.5',
       license = "GPL",
       description = "A set of handy Python fitsfile-related utility functions for DESDM",
       author = "Felipe Menanteau, Eric Neilsen",
