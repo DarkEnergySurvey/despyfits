b37dc3862164b2a5fab59758ea4e35bac78e8f88
Felipe Menanteau <felipe@illinois.edu>
2016-06-03 15:58:47 +0000

    Tagging despyfits trunk as 0.3.9

--- python/despyfits/__init__.py
+++ python/despyfits/__init__.py
@@ -1,5 +1,5 @@
 __author__  = "Felipe Menanteau, Eric Neilsen"
-__version__ = '0.3.5'
+__version__ = '0.3.9'
 version = __version__
 
 """
--- source_me_before_compile.sh
+++ source_me_before_compile.sh
@@ -3,6 +3,6 @@
 setup -v pyfits 3.3+3
 setup -v fitsio 0.9.8rc1+3 
 setup -v despyastro 0.3.7+0 
-setup -v despymisc 1.0.2+0
+setup -v despymisc 1.0.3+0
 setup -v shlib 2.1.11+2 
 # setup -v imsupport 7.3.2+1
