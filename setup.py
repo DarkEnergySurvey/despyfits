1bb8f217fc6b776476d163327e26818f3bcc66e8
Felipe Menanteau <felipe@illinois.edu>
2016-06-03 15:58:03 +0000

    Preparing for new despyfits tag and getting updating eups definitions for python 2.7.9+1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ libcompressionhdu = SharedLibrary(
 
 # The main call
 setup(name='despyfits',
-      version ='0.3.5',
+      version ='0.3.9',
       license = "GPL",
       description = "A set of handy Python fitsfile-related utility functions for DESDM",
       author = "Felipe Menanteau, Eric Neilsen",
